fd2e6081fc82b438107c85e0d7659d7eb3817d38
RaiaN <raianknight@gmail.com>
2014-01-01 06:43:10 -0800

    Access to filesystem

--- src/com/lebs/MainActivity.java
+++ src/com/lebs/MainActivity.java
@@ -5,10 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
     private ListView listView;
+    private Button getFilesButton;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +37,15 @@ public class MainActivity extends Activity {
                 intent.putExtra("song", song);
 
                 MainActivity.this.startActivity(intent);
+            }
+        });
+
+        getFilesButton = (Button)findViewById(R.id.getFilesButton);
+        getFilesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent filesystemIntent = new Intent(MainActivity.this,FilesystemActivity.class);
+                startActivity(filesystemIntent);
             }
         });
     }
