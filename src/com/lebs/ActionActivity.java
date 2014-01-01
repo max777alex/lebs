ee23acf15ec62e88c9a7a0d1960fc85e1d592c68
max777alex <max777alex@gmail.com>
2014-01-01 21:47:59 +0700

    Some music interaction added

--- src/com/lebs/ActionActivity.java
+++ src/com/lebs/ActionActivity.java
@@ -2,11 +2,15 @@ package com.lebs;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class ActionActivity extends Activity {
     EditText editText;
+    Button button;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +22,29 @@ public class ActionActivity extends Activity {
         String song = myIntent.getStringExtra("song");
 
         editText.setText(song);
+
+        button = (Button) findViewById(R.id.play);
+
+        button.setOnClickListener(new PlayButtonClickListener());
+    }
+
+    class PlayButtonClickListener implements View.OnClickListener {
+        MediaPlayer player;
+
+        @Override
+        public void onClick(View v) {
+            new Thread(new Runnable() {
+                public void run() {
+                    if(player == null || !player.isPlaying()) {
+                        player = MediaPlayer.create(ActionActivity.this, R.raw.numb_encore);
+                        player.start();
+                    }
+                    else {
+                        player.stop();
+                        player.release();
+                    }
+                }
+            }).start();
+        }
     }
 }
