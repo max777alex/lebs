3295cc9581fa1bd4eb86cfe3ac1d5d3c50761613
max777alex <max777alex@gmail.com>
2014-01-02 20:03:40 +0700

    Songs playing fixed

--- src/com/lebs/ActionActivity.java
+++ src/com/lebs/ActionActivity.java
@@ -2,49 +2,83 @@ package com.lebs;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class ActionActivity extends Activity {
     EditText editText;
-    Button button;
+    Button buttonPlay;
+    Button buttonStop;
+    MediaPlayer player;
+    Uri myUri;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.action_layout);
 
         editText = (EditText) findViewById(R.id.editText);
+        buttonPlay = (Button) findViewById(R.id.play);
+        buttonStop = (Button) findViewById(R.id.stop);
 
         Intent myIntent = getIntent();
         String song = myIntent.getStringExtra("song");
-
         editText.setText(song);
+        myUri = Uri.parse(myIntent.getStringExtra("path"));
 
-        button = (Button) findViewById(R.id.play);
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
 
-        button.setOnClickListener(new PlayButtonClickListener());
+            public void onClick(View v) {
+                if(player != null)
+                    player.reset();
+                else
+                {
+                    player = new MediaPlayer();
+                    player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                }
+
+                try {
+                    player.setDataSource(getApplicationContext(), myUri);
+                } catch (IllegalArgumentException e) {
+                    Toast.makeText(ActionActivity.this, "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
+                } catch (SecurityException e) {
+                    Toast.makeText(ActionActivity.this, "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
+                } catch (IllegalStateException e) {
+                    Toast.makeText(ActionActivity.this, "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    player.prepare();
+                } catch (IllegalStateException e) {
+                    Toast.makeText(ActionActivity.this, "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    Toast.makeText(ActionActivity.this, "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
+                }
+                player.start();
+            }
+        });
+
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                if(player != null && player.isPlaying()){
+                    player.stop();
+                }
+            }
+        });
     }
 
-    class PlayButtonClickListener implements View.OnClickListener {
-        MediaPlayer player;
-
-        @Override
-        public void onClick(View v) {
-            new Thread(new Runnable() {
-                public void run() {
-                    if(player == null || !player.isPlaying()) {
-//                        player = MediaPlayer.create(ActionActivity.this, R.raw.numb_encore);
-//                        player.start();
-                    }
-                    else {
-//                        player.stop();
-//                        player.release();
-                    }
-                }
-            }).start();
-        }
+    public void onPause() {
+        super.onPause();
+        if(player != null)
+            player.release();
     }
 }
