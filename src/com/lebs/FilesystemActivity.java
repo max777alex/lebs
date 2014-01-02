74793d4e0c6409016ecc99c7c35fc87d0efc0bf6
RaiaN <raianknight@gmail.com>
2014-01-02 00:58:25 -0800

    while-loop changes (more safe)

--- src/com/lebs/FilesystemActivity.java
+++ src/com/lebs/FilesystemActivity.java
@@ -16,14 +16,56 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class FilesystemActivity extends Activity {
-    private Button showFilesButton;
-    private ListView filesListView;
-
     private int FILENAME_IND = 0;
     private int PATH_IND = 1;
 
+    private Button showFilesButton;
+    private ListView filesListView;
+
     ArrayList<String> songs = new ArrayList<String>();
     ArrayList<String> songsPaths = new ArrayList<String>();
+
+    private Cursor getMusicCursor() {
+        String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
+        String[] projection = {
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DATA
+        };
+
+        ContentResolver resolver = FilesystemActivity.this.getContentResolver();
+        Cursor cursor = resolver.query(
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                projection,
+                selection,
+                null,
+                null);
+        return cursor;
+    }
+
+    private void listMusic(Cursor cursor) {
+        if( cursor == null ) {
+            return;
+        }
+        songs.clear();
+        songsPaths.clear();
+        while( cursor.getCount() > 0 && cursor.moveToNext() ){
+            songs.add(cursor.getString(FILENAME_IND));
+            songsPaths.add(cursor.getString(PATH_IND));
+        }
+
+        Context appContext = getApplicationContext();
+        if( appContext != null ) {
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(appContext, R.layout.item_layout,
+                    R.id.label,
+                    songs);
+            filesListView.setAdapter(adapter);
+        }
+    }
+
+    private void getMusic() {
+        Cursor cursor = getMusicCursor();
+        listMusic(cursor);
+    }
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,39 +74,10 @@ public class FilesystemActivity extends Activity {
         showFilesButton = (Button) findViewById(R.id.showFilesButton);
         filesListView = (ListView) findViewById(R.id.filesListView);
 
-
-
         showFilesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
-
-                String[] projection = {
-                        MediaStore.Audio.Media.TITLE,
-                        MediaStore.Audio.Media.DATA
-                };
-
-                ContentResolver resolver = FilesystemActivity.this.getContentResolver();
-                Cursor cursor = resolver.query(
-                        MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                        projection,
-                        selection,
-                        null,
-                        null);
-
-                songs.clear();
-                songsPaths.clear();
-                while(cursor.moveToNext()){
-                    songs.add(cursor.getString(FILENAME_IND));
-                    songsPaths.add(cursor.getString(PATH_IND));
-                }
-                Context appContext = getApplicationContext();
-                if( appContext != null ) {
-                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(appContext, R.layout.item_layout,
-                                                                                    R.id.label,
-                                                                                    songs);
-                    filesListView.setAdapter(adapter);
-                }
+                getMusic();
             }
         });
 
