94eec521f4e0c803eec5936bd4d68870a74d28ca
max777alex <max777alex@gmail.com>
2014-01-05 16:32:46 +0700

    artist field added into playlist item

--- src/com/lebs/PlayListItemArrayAdapter.java
+++ src/com/lebs/PlayListItemArrayAdapter.java
@@ -5,8 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -24,10 +26,13 @@ public class PlayListItemArrayAdapter extends ArrayAdapter<Song> {
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View itemView = inflater.inflate(R.layout.playlist_item, parent, false);
-        TextView textView = (TextView) itemView.findViewById(R.id.label);
+
+        TextView songNameTextView = (TextView) itemView.findViewById(R.id.songName);
+        TextView songArtistTextView = (TextView) itemView.findViewById(R.id.songArtist);
         ImageView imageView = (ImageView) itemView.findViewById(R.id.icon);
 
-        textView.setText(values.get(position).toString());
+        songNameTextView.setText(values.get(position).name);
+        songArtistTextView.setText(values.get(position).artist);
         imageView.setImageResource(R.drawable.play_icon);
 
         return itemView;
